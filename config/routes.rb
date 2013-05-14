708fa95ef2abced7f6a9d2fca6301acfd81a04ff
Evan Miller <evanmiller67@gmail.com>
2013-05-13 23:27:42 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ SampleApp::Application.routes.draw do
   match '/signup',   to: 'users#new'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
